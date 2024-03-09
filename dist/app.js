40098a1c0d857c11b55b5e200a749d3a5a9116fe
giusmili <giusmili67@gmail.com>
2024-03-09 19:22:37 +0100

    js

--- dist/app.js
+++ dist/app.js
@@ -1,27 +1,27 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", (e) => {
-    console.log(e.target);
-    console.log("Hello typescript et js");
-    const nom = "Typescript";
-    const version = 6;
-    const title = document.querySelector("title");
-    title.innerText = nom;
-    const Persona_1 = {
-        first_name: "John",
-        last_name: "Konig",
-        fiction: "Space 1999",
-        location: "Moon base Alpha",
-    };
-    const Persona_2 = {
-        first_name: "Helena",
-        last_name: "Russel",
-        fiction: "Space 1999",
-        location: "Moon base Alpha",
-    };
-    let characteres = [Persona_1, Persona_2];
-    characteres.push();
-    //console.log(characteres)
-    /* itération des objets dans le tableau */
-    //xcharacteres.forEach((element:any) => console.log(element.first_name));
-    characteres.map((element) => console.table(element));
+  console.log(e.target);
+  console.log("Hello typescript et js");
+  const nom = "Typescript";
+  const version = 6;
+  const title = document.querySelector("title");
+  title.innerText = nom;
+  const Persona_1 = {
+    first_name: "John",
+    last_name: "Konig",
+    fiction: "Space 1999",
+    location: "Moon base Alpha",
+  };
+  const Persona_2 = {
+    first_name: "Helena",
+    last_name: "Russel",
+    fiction: "Space 1999",
+    location: "Moon base Alpha",
+  };
+  let characteres = [Persona_1, Persona_2];
+  characteres.push();
+  //console.log(characteres)
+  /* itération des objets dans le tableau */
+  //xcharacteres.forEach((element:any) => console.log(element.first_name));
+  characteres.map((element) => console.table(element));
 });
